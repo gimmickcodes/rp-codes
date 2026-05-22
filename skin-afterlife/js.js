614ea055059900d5da63fa3def6b58718c0859ea
gimmickcodes <65742212+gimmickcodes@users.noreply.github.com>
2026-05-22 19:12:01 -0400

    some mobile

--- skin-afterlife/js.js
+++ skin-afterlife/js.js
@@ -16,6 +16,9 @@ $(".recent-topics-info").each(function () {
 
 
 
+// Hamburger menu on mobile
+// by Quartz 
+
 $(document).ready(function () {
    const clicktodismiss = $(document.createElement("div")).hide().attr("id", "clicktodismiss").attr("onclick", "menuFunction()")
    $(document.body).append(clicktodismiss);
@@ -23,9 +26,6 @@ $(document).ready(function () {
    $('#navstrip').prepend('<div class="topnav-themetoggle" onclick="toggleTheme()"><i class="ph ph-sun"></i><i class="ph ph-moon-stars"></i></div>');
 
 });
-
-// Hamburger menu on mobile
-// by Quartz 
 
 function menuFunction() {
    const hamburger = document.getElementById("navbar");
@@ -78,3 +78,88 @@ window.addEventListener('scroll', () => {
    fadeTarget.style.setProperty('--scroll-blur', blur);
 
 }, { passive: true }); // passive: true optimizes scroll performance
+
+
+
+// Guidebook tabs
+// Javascript to enable link to tab
+// Adapted from https://codepen.io/ciprian/pen/WEwPop
+
+// find page hash
+var hash = document.location.hash;
+var hashString = hash.replace('#', '');
+var tabLinks = document.querySelectorAll('.gb-nav a');
+var sections = document.querySelectorAll('.gb-page');
+
+for (var i = 0; i < tabLinks.length; i++) {
+   // if hash matches guidebook a tab link, mark tab active
+   // if hash is empty, make first tab active
+   if (tabLinks[i].getAttribute('href') == hash) {
+      $(tabLinks[i]).addClass('is-selected')
+      $(document.getElementById(hashString)).show()
+      $(document.getElementById(hashString)).addClass('active')
+
+   } if (hash == '') {
+      $(tabLinks[0]).addClass('is-selected')
+      $(sections[0]).show()
+      $(sections[0]).addClass('active')
+
+   }
+
+   // add click toggle to each links
+   tabLinks[i].addEventListener('click', function (e) {
+      // Remove active class from all links then add it to the one clicked
+      tabLinks.forEach(el => el.removeAttribute('class'))
+
+      // Hide all content tabs by default
+      for (var j = 0; j < sections.length; j++) {
+         sections[j].style.display = 'none';
+         $(sections[j]).removeClass('active');
+      }
+
+      // mark clicked link active
+      // return href value of clicked link as a string
+      // then show the tab content with matching ID
+      $(this).addClass('is-selected')
+      var tabID = this.getAttribute('href').replace('#', '');
+      $(document.getElementById(tabID)).show()
+      $(document.getElementById(tabID)).addClass('active')
+
+      // prevent default behavior of jumping to anchor point
+      e.preventDefault();
+      history.pushState({}, "", this.href);
+
+   })
+};
+
+// Guidebook nav on mobile
+
+var gbTabsToggle = document.querySelector('.gb-nav-toggle');
+var gbTabsToggleFiller = document.querySelector('.gb-nav-toggle-placehold-content');
+var gbNavTabs = document.querySelectorAll('.gb-nav a');
+
+// Populate dropdown toggle with the name of the selected tab
+gbTabsToggleFiller.innerHTML = document.getElementsByClassName('is-selected')[0].innerHTML;
+
+// Open dropdown menu on clicking toggle
+gbTabsToggle.addEventListener('click', function (e) {
+   document.querySelector('.gb-nav-links').classList.toggle('-is-open')
+
+   $(this).toggleClass('-is-open')
+});
+
+for (var i = 0; i < gbNavTabs.length; i++) {
+   gbNavTabs[i].addEventListener('click', function (e) {
+
+      // Close dropdown menu when clicking another tab
+      $(this).parent().parent().removeClass('-is-open')
+
+      // Re-populate dropdown toggle with name of newly selected tab
+      var selectedTabName = document.getElementsByClassName('is-selected')[0].innerHTML
+      gbTabsToggleFiller.innerHTML = selectedTabName;
+
+      $(gbTabsToggle).toggleClass('-is-open')
+
+
+   });
+}
